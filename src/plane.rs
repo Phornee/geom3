aa822c6605725a2166af472f07bf1aad8a7417b2
Phornee <phornee@gmail.com>
2025-03-17 17:14:15 +0100

    closest intersection & Shape trait

--- src/plane.rs
+++ src/plane.rs
@@ -1,3 +1,4 @@
+use crate::base::Shape;
 use crate::line3::Line3;
 use list::List;
 use std::fmt;
@@ -6,7 +7,7 @@ use vector3::Vector3;
 pub struct Plane {
     pub a: Vector3, // Pivot point of plane
     pub n: Vector3, // Normal of the Plane (already normalized)
-    pub d: f64      //"Independent" term precalculated for performance
+    pub d: f64,     //"Independent" term precalculated for performance
 }
 
 impl Plane {
@@ -19,12 +20,16 @@ impl Plane {
         Self {
             a: *a,
             n: n.normalize(),
-            d: -n.dot(&a)
+            d: -n.dot(&a),
         }
     }
 
+
+}
+
+impl Shape for Plane {
     /// Returns the normal (normalized) of the plane at a given point.
-    pub fn normal(&self, _point: &Vector3) -> Vector3 {
+    fn normal(&self, _point: &Vector3) -> Vector3 {
         self.n
     }
 
@@ -33,24 +38,31 @@ impl Plane {
     /// If the line is in the plane, it returns an empty list as a convention (because really, all lambdas fulfill).
     /// If the line intersects the plane, it returns a list with the lambda value.
     /// The lambda value is used to calculate the point of intersection with the line.calc_point(lambda).
-    pub fn intersects(&self, line: &Line3) -> List<f64> {
-        let mut intersections = List::<f64>::new();
+    fn intersects(&self, line: &Line3) -> List<f64> {
+        let mut intersections: List<f64> = List::<f64>::new();
 
-        let denom = self.n.dot(&line.v);
+        let denom: f64 = self.n.dot(&line.v);
         if denom != 0. {
-            let num = - self.n.dot(&line.a) - self.d;
+            let num: f64 = -self.n.dot(&line.a) - self.d;
             intersections.push(num / denom);
         }
         intersections
     }
-}
 
-// impl fmt::Debug for Plane {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         //f.debug_struct("Line3").field("a", &self.a).field("v", &self.v).finish()
-//          f.write_str(&format!("({}, {}, {}) --> ({}, {}, {})", self.a.x, self.a.y, self.a.z, self.v.x, self.v.y, self.v.z))
-//     }
-// }
+    /// Returns the intersection of the plane with a line.
+    /// If the line is parallel to the plane, it returns Option None.
+    /// If the line is in the plane, it returns Option None as a convention (because really, all lambdas fulfill).
+    /// If the line intersects the plane, it returns the lambda value.
+    /// The lambda value is used to calculate the point of intersection with the line.calc_point(lambda).
+    fn closest_intersection(&self, line: &Line3) -> Option<f64> {
+        let denom: f64 = self.n.dot(&line.v);
+        if denom != 0. {
+            let num: f64 = -self.n.dot(&line.a) - self.d;
+            return Some(num / denom);
+        }
+        return None;
+    }
+}
 
 impl fmt::Display for Plane {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
--- src/triangle3.rs
+++ src/triangle3.rs
@@ -1,4 +1,5 @@
 use vector3::Vector3;
+use crate::base::Shape;
 use crate::plane::Plane;
 use crate::line3::Line3;
 use list::List;
@@ -32,43 +33,15 @@ impl Triangle3 {
         let ab: Vector3 = *b - *a;
         let ac: Vector3 = *c - *a;
 
-        let n = ab.cross(&ac).normalize();
-        let plane = Plane::new(&a, &n);
+        let n: Vector3 = ab.cross(&ac).normalize();
+        let plane: Plane = Plane::new(&a, &n);
 
-        let d00 = ab.dot(&ab);
-        let d01 = ab.dot(&ac);
-        let d11 = ac.dot(&ac);
-        let denom =  d00 * d11 - d01 * d01;
+        let d00: f64 = ab.dot(&ab);
+        let d01: f64 = ab.dot(&ac);
+        let d11: f64 = ac.dot(&ac);
+        let denom: f64 =  d00 * d11 - d01 * d01;
 
         Self { a: *a, b: *b, c: *c, plane, ab, ac, d00, d01, d11, denom }
-    }
-
-    /// Returns the normal (normalized) of the triangle at a given point.
-    pub fn normal(&self, _point: &Vector3) -> Vector3 {
-        self.plane.n
-    }
-
-    /// Returns the intersection of the triangle with a line.
-    /// If the line is parallel to the triangle, it returns an empty list.
-    /// If the line is in the plane of the triangle, it returns an empty list as a convention (because really, all lambdas fulfill).
-    /// If the line intersects the triangle, it returns a list with the lambda value.
-    pub fn intersects(&self, line: &Line3) -> List::<f64> {
-        let intersections = self.plane.intersects(line);
-        if intersections.iter().count() == 0 {
-            return intersections;
-        }
-        else {
-            let p = line.calc_point(*intersections.iter().next().unwrap());
-            let b = self.barycentric(&p);
-            if  1.0 >= b.x && b.x >= 0.0 && 
-                1.0 >= b.y && b.y >= 0.0 && 
-                1.0 >= b.z && b.z >= 0.0 {
-                return intersections;
-            }
-            else {
-                return List::<f64>::new();
-            }
-        }
     }
 
     /// Returns the barycentric coordinates of a point in the triangle.
@@ -82,23 +55,70 @@ impl Triangle3 {
     /// If the point is inside the triangle, the coordinates are between 0 and 1.
     /// The point (or any other value associated to the vertexes) is calculated as `p = a * bar_a + b * bar_b + c * bar_c`.
     pub fn barycentric(&self, p: &Vector3) -> Vector3 {
-        let ap = *p - self.a;
-        let d20 = ap.dot(&self.ab);
-        let d21 = ap.dot(&self.ac);
-        let bar_b = (self.d11 * d20 - self.d01 * d21) / self.denom;
-        let bar_c = (self.d00 * d21 - self.d01 * d20) / self.denom;
-        let bar_a = 1.0 - bar_b - bar_c;
+        let ap: Vector3 = *p - self.a;
+        let d20: f64 = ap.dot(&self.ab);
+        let d21: f64 = ap.dot(&self.ac);
+        let bar_b: f64 = (self.d11 * d20 - self.d01 * d21) / self.denom;
+        let bar_c: f64 = (self.d00 * d21 - self.d01 * d20) / self.denom;
+        let bar_a: f64 = 1.0 - bar_b - bar_c;
         
         Vector3{x: bar_a, y: bar_b, z: bar_c }
     }
+
 }
 
-// impl fmt::Debug for Triangle3 {
-//     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-//         //f.debug_struct("Line3").field("a", &self.a).field("v", &self.v).finish()
-//          f.write_str(&format!("({}, {}, {}) --> ({}, {}, {})", self.a.x, self.a.y, self.a.z, self.v.x, self.v.y, self.v.z))
-//     }
-// }
+impl Shape for Triangle3 {
+    /// Returns the normal (normalized) of the triangle at a given point.
+    fn normal(&self, _point: &Vector3) -> Vector3 {
+        self.plane.n
+    }
+
+    /// Returns the intersection of the triangle with a line.
+    /// If the line is parallel to the triangle, it returns an empty list.
+    /// If the line is in the plane of the triangle, it returns an empty list as a convention (because really, all lambdas fulfill).
+    /// If the line intersects the triangle, it returns a list with the lambda value.
+    fn intersects(&self, line: &Line3) -> List::<f64> {
+        let intersections: List<f64> = self.plane.intersects(line);
+        if intersections.iter().count() == 0 {
+            return intersections;
+        }
+        else {
+            let p: Vector3 = line.calc_point(*intersections.iter().next().unwrap());
+            let b: Vector3 = self.barycentric(&p);
+            if  1.0 >= b.x && b.x >= 0.0 && 
+                1.0 >= b.y && b.y >= 0.0 && 
+                1.0 >= b.z && b.z >= 0.0 {
+                return intersections;
+            }
+            else {
+                return List::<f64>::new();
+            }
+        }
+    }
+
+    /// Returns the intersection of the triangle with a line.
+    /// If the line is parallel to the triangle, it returns an empty list.
+    /// If the line is in the plane of the triangle, it returns an empty list as a convention (because really, all lambdas fulfill).
+    /// If the line intersects the triangle, it returns a list with the lambda value.
+    fn closest_intersection(&self, line: &Line3) -> Option<f64> {
+        let intersection: Option<f64> = self.plane.closest_intersection(line);
+        if intersection.is_none() {
+            return intersection;
+        }
+        else {
+            let p: Vector3 = line.calc_point(intersection.unwrap());
+            let bar: Vector3 = self.barycentric(&p);
+            if  1.0 >= bar.x && bar.x >= 0.0 && 
+                1.0 >= bar.y && bar.y >= 0.0 && 
+                1.0 >= bar.z && bar.z >= 0.0 {
+                return intersection;
+            }
+            else {
+                return None;
+            }
+        }
+    }
+}
 
 // impl fmt::Display for Triangle3 {
 //     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
